2ca632fe89bf007903a3df171c738568ae94cb0f
silloi <silloi1993@gmail.com>
2022-07-31 14:51:41 +0900

    chapter 06

---
+++ ex/ex06/ex06_1.ml
@@ -0,0 +1,11 @@
+let square x = x * x
+
+(* square _3._ ;; *)
+
+let pi = 3.1415
+(* let circle = 2. *. pi *. _r_ ;; *)
+(* circle 2. ;; *)
+
+(* let _TV_ bangumi youbi =
+   bangumi ^ "は" ^ youbi ^ "に放映です。" ;; *)
+(* TV "ポケモン" "木曜日" ;; *)
